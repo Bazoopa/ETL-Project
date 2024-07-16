6603a5b103b6a6a8528cf018e3cca2cb8fa27044
Bazoopa <37423252+Bazoopa@users.noreply.github.com>
2024-07-16 11:13:03 +0100

    uploading the files
    uploading all the files that make up the project. More work needs done on this when time allows. Would be good to have Python Poetry running for example.

---
+++ etl/data_processors/DataProcessor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from etl.extractor import Extractor
+
+
+class DataProcessor:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.data = self.extract_data()
+        self.df = pd.DataFrame()
+
+    def extract_data(self):
+        # @todo: It would be cool to add the functionality here so that it doesn't matter whether you input a CSV or a JSON file, the method can just deal with both.
+        return Extractor.Extractor.fromCSV(self.file_name)
+
+    # @todo: change this so that it uses the Loader Class methods.
+    def load_data(self, csv_file_name):
+        self.df.to_csv(csv_file_name)
